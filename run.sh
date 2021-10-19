932c5db98801d342087300c52985e4c570bbe954
Jack O'Donoghue <jackcodonoghue@gmail.com>
2021-10-19 09:14:14 -0600

    Data In

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-python3 DecisionTree/ID3.py
+#!/bin/bash
+
+arg="$1"
+
+case "$arg" in
+    0 ) python3 EnsembleLearning/Adaboost.py 0;;
+    1 ) python3 EnsembleLearning/BaggedTrees.py 1;;
+    2 ) python3 EnsembleLearning/BaggedTrees.py 2;;
+    3 ) python3 EnsembleLearning/RandomForests.py 3;;
+    4 ) python3 EnsembleLearning/RandomForests.py 4;;
+    5 ) python3 LinearRegression/LinearRegression.py 5;;
+    6 ) python3 LinearRegression/LinearRegression.py 6;;
+esac
